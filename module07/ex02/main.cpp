3c67ab9067c18b39117edc9b7367f2e3e7e6f8d3
f9lcon <f9lcon@yandex.ru>
2022-03-09 21:11:46 +0300

    end of day 7

--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
-#include "iter.hpp"
+#include "Array.hpp"
 
 
 int main( void )
 {
-
+	Array<int> a(5);
+	a[0] = 42;
+	a[1] = 21;
+	std::cout << a[0] << std::endl;
+	std::cout << a[1] << std::endl;
+	try
+	{
+		std::cout << a[10] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	Array<int> b(a);
+	std::cout << b[0] << std::endl;
+	std::cout << b[1] << std::endl;
+	try
+	{
+		std::cout << b[10] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	Array<int> c;
+	c = b;
+	std::cout << c;
 	return (0);
 }
